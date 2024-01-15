00067929c58ef96ed288aafe11c8c3e4a43e3cf5
Bayukgan <Even.Ytterli.Tokle@gmail.com>
2024-01-15 02:03:06 +0100

    Feat: Add way to create Matrix from dictionary

--- Matrix.py
+++ Matrix.py
@@ -1,9 +1,25 @@
 class Matrix():
     'This is a custom made matrix class'
-    def __init__(self, *rows):
+    def __init__(self, *rows, dictionary={}, n=0, m=0):
+        self.dict = dictionary.copy()
+        self.n = n
+        self.m = m
 
-        self.dict = {}
-        self.tDict = {}
+        if len(rows) > 0:
+            self.newMatrix(rows)
+        elif self.n == 0 or self.m == 0:
+            self.n = max([coord[0] for coord in self.dict.keys()]) + 1
+            self.m = max([coord[1] for coord in self.dict.keys()]) + 1
+
+
+        if not all(isinstance(val, int) or isinstance(val, float) for val in self.dict.values()):
+            wrongTypes = [type(val) for val in self.dict.values() if type(val) != int and type(val) != float]
+            raise TypeError(
+                f'Unsupported type, {str(wrongTypes[0])[7:-1]}, in object creation'
+            )
+            
+    
+    def newMatrix(self, rows):
         self.n = len(rows)
         self.m = set()
         
@@ -18,17 +34,11 @@ class Matrix():
                 )
     
         self.m = int(list(self.m)[0])
-        
-        if not all(isinstance(val, int) or isinstance(val, float) for key, val in self.dict.items()):
-            wrongTypes = [type(val) for key, val in self.dict.items() if type(val) != int and type(val) != float]
-            raise TypeError(
-                f'Unsupported type, {str(wrongTypes[0])[7:-1]}, in object creation'
-            )
-            
+
 
     def __repr__(self):
         ret = ''
-        # ret += f'Matrix{self.m, self.n}\n'
+        # ret += f'Matrix{self.n, self.m}\n'
         for i in range(self.n):
             ret += '│'
             for j in range(self.m):
@@ -53,5 +63,11 @@ def nullMatrix(n, m):
 
 
 x = Matrix((1, 2, 4), [3, 4, 5], (5, 6, 7))
+y = nullMatrix(2, 5)
+z = Matrix(dictionary=x.dict, n=x.n, m=x.m)
+i = idMatrix(4)
 
 print(x)
+print(y)
+print(z)
+print(i)
